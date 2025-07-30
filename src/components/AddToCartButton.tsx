2168b07e8348ebf08fbf3f2000bb5f92c171f034
shreyas367 <shreyas367@users.noreply.github.com>
2025-07-30 21:26:32 +0530

    dwda

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -22,7 +22,7 @@ export default function AddToCartButton({ product }: Props) {
             title: product.title,
             price: product.price,
             quantity: 1,
-            image: ''
+            image: product.image|| "" ,
         })
       }
       className="bg-black text-white px-4 py-2 rounded mt-4"
